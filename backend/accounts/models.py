fd2248158faf5a260d5a1addba77dcc1c953a8d7
gahui <jess12243648@gmail.com>
2025-11-21 17:01:23 +0900

    Feat: User, Article, Interaction models and settings

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField(max_length=50, blank=True)
+    interests = models.CharField(max_length=200, blank=True, default="")
+
+    def __str__(self):
+        return self.username
